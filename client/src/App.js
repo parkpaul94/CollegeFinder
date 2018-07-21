dba0b4e0743b08b4b24c7478e8abe8c4c72e6f3e
David <dchen95@terpmail.umd.edu>
2018-07-21 10:37:32 -0400

    added react front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,19 @@
-import React  from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./Pages/Home";
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
-import Login from './pages/Login';
-import SignupForm from './components/SignupForm'
-
-import NoMatch from './pages/NoMatch';
 
 const App = () => (
-  <Router>
-      <Switch>
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/signup' component={SignupForm} />
-        {/* <Route exact path='/books' component={Books} />
-        <Route exact path='/books/:id' component={Detail} /> */}
-        <Route component={NoMatch} />
-      </Switch>
-  </Router>
-)
+	<Router>
+		<div>
+      <Nav />
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/Home" component={Home} />
+			</Switch>
+		</div>
+	</Router>
+);
 
 export default App;
