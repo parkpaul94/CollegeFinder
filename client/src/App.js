2a257604691746e46fc7eac8a7eb351e4b9b5f6b
Eric J <34559118+jiapurdue@users.noreply.github.com>
2018-07-21 11:43:17 -0400

    fix merge issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
-import Home from "./Pages/Home";
+import Home from "./pages/Home";
 
-<<<<<<< HEAD
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
-import Login from './pages/Login';
-import Home from './pages/Home'
-
-import NoMatch from './pages/NoMatch';
-
-const App = () => (
-  <Router>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/login' component={Login} />
-        {/* <Route exact path='/signup' component={SignupForm} /> */}
-        {/* <Route exact path='/books' component={Books} />
-        <Route exact path='/books/:id' component={Detail} /> */}
-        <Route component={NoMatch} />
-      </Switch>
-  </Router>
-)
-=======
 
 const App = () => (
 	<Router>
@@ -36,6 +15,5 @@ const App = () => (
 		</div>
 	</Router>
 );
->>>>>>> master
 
 export default App;
