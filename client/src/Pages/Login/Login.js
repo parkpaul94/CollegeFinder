2fe5e09412fef63927b8074c646000ba627de130
Eric J <34559118+jiapurdue@users.noreply.github.com>
2018-07-22 16:48:14 -0400

    update components

--- client/src/Pages/Login/Login.js
+++ client/src/Pages/Login/Login.js
@@ -1,12 +1,17 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
+import axios from 'axios'
+
 import Login_Form from "../../components/Login_Form/Login_Form";
+import Nav from "../../components/Nav";
 
 class Login extends Component {
 	render() {
 		return (
-            <Login_Form />
+			<Nav>
+				<Login_Form />
+			</Nav>
 		);
 	}
-}
+};
 
 export default Login;
--- client/src/Pages/Signup/Signup.js
+++ client/src/Pages/Signup/Signup.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import Signup_Form from "../../components/Signup_Form/Signup_Form";
+import Nav from "../../components/Nav";
 
 class Signup extends Component {
 	render() {
 		return (
-            <Signup_Form />
+				<Signup_Form />
 		);
 	}
 }
