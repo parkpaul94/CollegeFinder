2cbb8515e2ec02de26fdf82a35a708bbaa1807fe
David <dchen95@terpmail.umd.edu>
2018-07-23 20:59:24 -0400

    added search page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,40 +1,50 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../components/Grid";
+import Button from '../../components/Button'
+import './Home.css';
+import Jumbotron from "../../components/Jumbotron"
 import Nav from "../../components/Nav";
 
 // import Jumbotron from "../../components/Jumbotron";
 // import API from "../../utils/API";
 
 class Home extends Component {
+
 	render() {
+		const style ={
+			fontSize:'2.5em'
+		};
+
+
 		return (
-			<Nav>
+		<div className="App">
 			<Container fluid>
 				<Row>
-					<Col size="md-12">
-						{/* <Jumbotron>
-                        <div class="col s6 one"><i class="material-icons logo">school</i></div>
-						</Jumbotron> */}
-					</Col>
-				</Row>
-				<Row>
-					<Col size="md-10 md-offset-1">
-						{/* <article>
-							<h1>Synopsis</h1>
-							<p>
-								{this.state.book.synopsis}
-							</p>
-						</article> */}
-					</Col>
-				</Row>
-				<Row>
-					<Col size="md-2">
-						<Link to="/">← Back to Authors</Link>
+				<Col size="md-7">
+					<img src={require("./College.jpg")}/>
+				</Col>
+					<Col size="md-5">
+						<Row/>
+						<Row>
+							<a style={style}>Looking for colleges is hard. 
+							We are here to help!
+							Let’s make your next 4 years 
+							an adventure to remember !!</a>
+						</Row>
+						<Row>
+							<Button type="primary" className="input-lg">
+							<Link to="/Search">Search For Colleges</Link> 
+							</Button>
+							
+							<Button type="primary" className="input-lg">
+							<Link to="/SignUp">Sign Up For An Account</Link>
+							</Button>
+						</Row>
 					</Col>
 				</Row>
 			</Container>
-			</Nav>
+		</div>
 		);
 	}
 }
